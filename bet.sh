0921216788d64ff24c78672dbc80078064850e49
Uday Suresh <udaysuresh1@gmail.com>
2018-04-13 14:38:12 -0700

    bet floating

--- bet.sh
+++ bet.sh
@@ -4,4 +4,7 @@ mkdir ./OUT
 rm ./OUT/*
 
 # initial conversion of scanner files from DICOM to Nifti files
-dcm2nii -o OUT [[DICOM SOURCE]]
+dcm2niix -o /OUT /path/to/DICOM/folder
+
+echo "now running BET protocol"
+bet /OUT/path/to/input/nifti extracted_data -f 0.3 -g 0 -m
