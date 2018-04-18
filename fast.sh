7c767ba3e83387e1748131de53fb6a905fe2eafa
Uday Suresh <udaysuresh1@gmail.com>
2018-04-18 12:53:53 -0700

    fix fast

--- fast.sh
+++ fast.sh
@@ -1,4 +1,4 @@
 #!/bin/tcsh
 
 echo "now running FAST protocol"
-fast $argv[2] -n 3 -H 0.1 -I 4 -l 20.0 -o ./fast_data ./bet_data
+fast -n 3 -H 0.1 -I 4 -l 20.0 -o ./segemented_data ./extracted_data
---
+++ flirt.sh
@@ -0,0 +1,2 @@
+# flirt needs to be run in reference to another image 
+# registration requires a reference
